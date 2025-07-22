dc22e5aff2b50b9fe0a937c31335f8321622f88d
Kauê Oliveira <kaueoliveira3002@gmail.com>
2025-07-22 08:58:18 -0300

    Delete Projetos-Semestrais/APS-1º/JS directory

--- Projetos-Semestrais/APS-1º/JS/Codigos.js
+++
@@ -1,6 +0,0 @@
-/*Função no botão Enviar do formulário de comentários na página Fórum*/
-  function comEnviado(){
-    var nome = document.getElementById('nome').value;
-    alert("Mensagem enviada com sucesso! Ela será verificada e publicada.");
-  }
-  
