bfe1765c7eecf58032fd293f30c42d1cf6abf393
Kauê Oliveira <kaueoliveira3002@gmail.com>
2025-07-22 08:53:18 -0300

    Add files via upload

---
+++ Projetos-Semestrais/APS-1º/JS/Codigos.js
@@ -0,0 +1,6 @@
+/*Função no botão Enviar do formulário de comentários na página Fórum*/
+  function comEnviado(){
+    var nome = document.getElementById('nome').value;
+    alert("Mensagem enviada com sucesso! Ela será verificada e publicada.");
+  }
+  
